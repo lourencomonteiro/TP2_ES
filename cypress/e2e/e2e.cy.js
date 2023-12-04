3ff3873accced9ab0c45c4f9c480e85a7f637d38
lourencomonteiro <lo.montenegro2002@gmail.com>
2023-12-04 20:38:32 -0300

    feat: testes e2e finalizados

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+describe("E2E tests", () => {
+
+  it("should be in login url", () => {
+    cy.visit("http://localhost:3000/");
+    cy.wait(2000);
+    cy.clearAllCookies();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
+
+  it("should be in login url", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.wait(2000);
+    cy.clearAllCookies();
+    cy.url().should("eq", "http://localhost:3000/login");
+  });
+
+  it("should be in login url", () => {
+    cy.visit("http://localhost:3000/register");
+    cy.wait(2000);
+    cy.clearAllCookies();
+    cy.url().should("eq", "http://localhost:3000/register");
+  });
+
+  it("should be in login url", () => {
+    cy.visit("http://localhost:3000/about", { failOnStatusCode: false });
+    cy.wait(2000);
+    cy.clearAllCookies();
+
+    // Verificar se o status da resposta é 404
+    cy.get("body").then(($body) => {
+      if ($body.find("h1").text().includes("404")) {
+        cy.log("Página retornou um erro 404.");
+      } else {
+        // Se não houver erro 404, verificar a URL
+        cy.url().should("eq", "http://localhost:3000/about");
+      }
+    });
+  });
+
+  it("should display 'Sticker Trader' in the logo text box", () => {
+    cy.visit("http://localhost:3000/"); // Substitua pela URL real da sua página
+    cy.wait(2000);
+    // Verificar se o texto 'Sticker Trader' está presente no elemento com a classe 'logo__textBox--text'
+    cy.get('.logo__textBox--text').should('contain', 'Sticker Trader');
+  });
+
+
+});
